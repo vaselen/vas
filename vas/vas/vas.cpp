d903beab5977e3779eb71c78e6b8df5608d5ed27
vaselen <elenasimf4@gmail.com>
2024-09-19 14:16:43 +0300

    меню

--- vas/vas/vas.cpp
+++ vas/vas/vas.cpp
@@ -1,23 +1,102 @@
-﻿// vas.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <iostream>
+#include <fstream>
+using namespace std;
 
-#include <iostream>
+struct Pipe
+{
+    string name = "None";
+    float length = 0.0;
+    int diameter = 0;
+    bool repair = false;
+};
+
+//Pipe AddPipe()
+//{
+//    Pipe new_pipe;
+//    cout << endl << "Adding a new pipe..." << endl;
+//    cout << "Enter the name of the pipe: ";
+//    cin.ignore();
+//    getline(cin, new_pipe.pipe_name);
+//    cout << "Enter the length of the pipe (in metres): ";
+//    check_double(new_pipe.pipe_length);
+//    cout << "Enter the pipe diameter (in millimetres): ";
+//    check_int(new_pipe.pipe_diameter);
+//    cout << "Enter the repair status: ";
+//    check_bool(new_pipe.pipe_repair);
+//    return new_pipe;
+//}
+
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
-    std::cout << "5";
-    std::cout << "6";
+    Pipe pipe0;
 
+    int option;
+    while (true) {
+        cout << "Choose command" << endl
+            << "1. Add pipe" << endl
+            << "2. Add CS" << endl
+            << "3. Show all objects" << endl
+            << "4. Edit pipe" << endl
+            << "5. Edit CS" << endl
+            << "6. Save" << endl
+            << "7. Download" << endl
+            << "8. Exit " << endl;
+        cin >> option;
+        if (cin.fail() || option < 1 || option > 8)
+        {
+            cout << " There is no such command" << endl;
+            cin.clear();
+            cin.ignore(1000, '\n');
+            continue;
+        }
+
+
+        switch (option)
+        {
+        case 1:
+        {
+            cout << "iwed" << endl;
+            break;
+        }
+        case 2:
+        {
+            cout << "jdku" << endl;
+            break;
+        }
+        case 3:
+        {
+            cout << "jeiw" << endl;
+            break;
+        }
+        case 4:
+        {
+            cout << "slwo" << endl;
+            break;
+        }
+        case 5:
+        {
+            cout << "slwo" << endl;
+            break;
+        }
+        case 6:
+        {
+            cout << "slwo" << endl;
+            break;
+        }
+        case 7:
+        {
+            cout << "slwo" << endl;
+            break;
+        }
+        case 8:
+        {
+            return false;
+            break;
+        }
+        }
+
+    }
+    return 0;
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
